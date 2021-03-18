7d9ca57f3c249c6cf4cae35fbd22a9fb05f2c527
Abhishek Tripathi <abhishektripathi@Abhisheks-MacBook-Air.local>
2021-03-19 02:27:13 +0530

    Final

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.json());
+app.use(express.json({ extended: false }));
 
 app.use('/users', usersRoutes);
 
